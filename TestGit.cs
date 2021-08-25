5f668673774e7967c12793aed6db6b08a612121b
Bannanroc <60995744+Bannanroc@users.noreply.github.com>
2021-08-25 20:16:48 +0700

    ADD-Hello

---
+++ TestGit.cs
@@ -0,0 +1,12 @@
+using Systems;
+
+namespace HelloWorld
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("dsada");
+        }
+    }
+}
